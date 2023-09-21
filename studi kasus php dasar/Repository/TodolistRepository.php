f15503da6b41d377588ba2ed7734b136b805779c
thumberlys10 <108513235+thumbs123@users.noreply.github.com>
2023-09-21 09:46:44 +0700

    halo

---
+++ studi kasus php dasar/Repository/TodolistRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Repository {
+
+    user Entity\Todolist;
+
+    interface TodolistRepository
+    {
+        
+        function save(Todolist $todolist): void;
+
+        function remove (int $number): bool;
+
+        function findAll(): array;
+    }
+
+    class TodolistRepositoryImpl implements TodolistRepository{
+
+        public array $todolist = array();
+
+        private \PDO $connection;
+
+        public function __construct(\PDO $connection)
+        {
+            $this->$connection = $connection
+        }
+
+        function save(Todolist $todolist): void
+        {
+            // $number = sizeof($this->todolist)+1;
+            // $this->todolist($number)=$todolist;
+
+            $sql = "INSERT INTO todolist(todo) VALUES (?)";
+            $statement = $this->connection->prepare($sql);
+            $statement->execute([$todolist->getTodo()]);
+            
+        }
+        if ($number > sizeof($this->todolist)){
+            return false;
+        }
+
+        for ($i = $number; $i < sizeof($this->todolist); $i++){
+            $this->todolist[$i] = $this->todolist[$i + 1];
+        }
+        unset($this->todolist[sizeof($this->todolist)]);
+
+        return true
+    }
+    function findAll(): array
+    {
+        return $this->todolist;
+    }
+}
